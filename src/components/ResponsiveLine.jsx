97178579e957b653f658c8479cf76e0e1a57a77e
Aishwarya K <aish3399@gmail.com>
2023-04-01 00:21:29 +0530

    ResponsiveBar.js, ResponsiveLine.js , App.js

---
+++ src/components/ResponsiveLine.jsx
@@ -0,0 +1,76 @@
+// LineChart.jsx
+
+import React from "react";
+import { ResponsiveLine } from "@nivo/line";
+import data from "../data";
+
+const LineChart = () => {
+  return (
+    <div style={{ height: "400px" }}>
+      <ResponsiveLine
+        data={data.lineChartData}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
+        curve="cardinal"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Date",
+          legendOffset: 36,
+          legendPosition: "middle"
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Value",
+          legendOffset: -40,
+          legendPosition: "middle"
+        }}
+        colors={{ scheme: "category10" }}
+        enableGridX={false}
+        enableGridY={false}
+        pointSize={10}
+        pointColor={{ theme: "background" }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "serieColor" }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
+    </div>
+  );
+};
+
+export default LineChart;
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+      id: "chart1",
+      data: [
+        { x: "A", y: 10 },
+        { x: "B", y: 20 },
+        { x: "C", y: 30 },
+        { x: "D", y: 15 },
+        { x: "E", y: 25 },
+      ],
+    },
+    {
+      id: "chart2",
+      data: [
+        { x: "Jan", y: 100 },
+        { x: "Feb", y: 200 },
+        { x: "Mar", y: 300 },
+        { x: "Apr", y: 150 },
+        { x: "May", y: 250 },
+      ],
+    },
+    {
+      id: "chart3",
+      data: [
+        { id: "A", label: "A", value: 10 },
+        { id: "B", label: "B", value: 20 },
+        { id: "C", label: "C", value: 30 },
+        { id: "D", label: "D", value: 15 },
+        { id: "E", label: "E", value: 25 },
+      ],
+    },
+  ];
+  
+  export default data;
